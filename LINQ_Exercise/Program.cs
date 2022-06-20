68caaf73a8b4331ef331d328e002c9394c593060
David Chuvik <dchuvik@gmail.com>
2022-06-20 15:51:32 -0400

    LINQ practice

---
+++ LINQ_Exercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> videoGameNames = new List<string>() { "Halo 2", "GTA V", "Super Mario Kart", "Need for Speed", "FIFA 20" };
+            var orderedList = videoGameNames.OrderBy(x => x.Length).ToList();
+
+            foreach (var item in orderedList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
